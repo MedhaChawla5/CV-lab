7f94d6329d36984cd28be5d6f4c6dbb80b116f8e
MedhaChawla5 <133873481+MedhaChawla5@users.noreply.github.com>
2024-08-03 16:04:00 +0530

    Delete q5.py

--- q5.py
+++
@@ -1,9 +0,0 @@
-import cv2
-image = cv2.imread('computer_vision.png')
-(h, w) = image.shape[:2]
-new_width = 100
-aspect_ratio = h / w
-new_height = int(new_width * aspect_ratio)
-resized_image = cv2.resize(image, (new_width, new_height))
-cv2.imshow("Resized image",resized_image)
-cv2.waitKey(0)
