c8140101e08fdd4faac2a738f95acf50d6ddf0fd
MedhaChawla5 <133873481+MedhaChawla5@users.noreply.github.com>
2024-08-03 16:03:42 +0530

    Delete q3.py

--- q3.py
+++
@@ -1,7 +0,0 @@
-import cv2
-image = cv2.imread("computer_vision.png")
-print(image.dtype)
-x,y,z = image.shape
-for i in range(10):
-    for j in range(10):
-        print("pixel number",i,",",j,"is",image[i][j])
