745899dcebebc1bf3d94bc01eeb99362b3a8bb37
MedhaChawla5 <133873481+MedhaChawla5@users.noreply.github.com>
2024-10-14 15:26:51 +0530

    Update q1.py

--- lab7/q1.py
+++ lab7/q1.py
@@ -5,26 +5,24 @@ import glob
 
 CHECKERBOARD = (12, 12)
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 50, 0.0001)
-objpoints = []  # 3D point in real world space
-imgpoints = []  # 2D points in image plane
 
-objp = np.zeros((CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
-objp[:, :2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
+objpoints = []
+imgpoints = []
+
+objp = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
+objp[0, :, :2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
 
 images = glob.glob('./calib_example/*.tif')
-
 for fname in images:
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD,
-        cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+    ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
 
-    if ret:
+    if ret == True:
         objpoints.append(objp)
         corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
         imgpoints.append(corners2)
-
         img = cv2.drawChessboardCorners(img, CHECKERBOARD, corners2, ret)
         cv2.imshow('img', img)
         cv2.waitKey(0)
@@ -33,8 +31,9 @@ cv2.destroyAllWindows()
 
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
-print("Camera matrix : \n", mtx)
-print("Distortion coefficients : \n", dist)
+print("Camera matrix (Intrinsic parameters): \n", mtx)
+print("Distortion coefficients: \n", dist)
+
 for i in range(len(rvecs)):
-    print(f"Rotation vector for image {i+1} : \n", rvecs[i])
-    print(f"Translation vector for image {i+1} : \n", tvecs[i])
+    print(f"Rotation vector {i}: \n", rvecs[i])
+    print(f"Translation vector {i}: \n", tvecs[i])
